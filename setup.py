ee57034dc707d35fd2c9bea647aa04665ca23dcf
Nikkol <nikol.tamayo@est.iudigital.edu.com>
2025-05-15 20:12:05 -0500

    Actualización Archivo

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 setup(
     name="edu_pad",
     version="0.0.1",
-    author="Juliana Pena",
-    author_email="juliana.pena@est.iudigital.edu.co",
+    author="Nikol Tamayo",
+    author_email="",
     description="",
     py_modules=["actividad1","actividad2"],
     install_requires=[
